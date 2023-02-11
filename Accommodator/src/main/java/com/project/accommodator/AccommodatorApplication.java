ca1fdf148d9358b229b7dca5471bd5538fc54b12
Fenil Patel <fenil.patel@dal.ca>
2023-02-11 15:13:31 -0400

    accommodator created

---
+++ Accommodator/src/main/java/com/project/accommodator/AccommodatorApplication.java
@@ -0,0 +1,13 @@
+package com.project.accommodator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccommodatorApplication.class, args);
+    }
+
+}
---
+++ Accommodator/src/test/java/com/project/accommodator/AccommodatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.accommodator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
