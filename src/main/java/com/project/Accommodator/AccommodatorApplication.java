d4a6a236e0a850280e66da33c2afdd9a2321c463
Fenil Patel <fenil.patel@dal.ca>
2023-02-15 18:01:20 -0400

    Back_End initialized

---
+++ src/main/java/com/project/Accommodator/AccommodatorApplication.java
@@ -0,0 +1,14 @@
+package com.project.Accommodator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodatorApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        SpringApplication.run(AccommodatorApplication.class, args);
+    }
+
+}
