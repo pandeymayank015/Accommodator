538efb9e15e460e0c14fb9c1267f95dec3ded4a2
fenilp <fenil.patel@dal.ca>
2023-04-01 16:11:43 -0300

    JWT Front_Integration with All bug fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
 
 test('renders learn react link', () => {
   render(<App />);
